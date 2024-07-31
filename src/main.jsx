15776168c65d2a57a34aec4943cf91c0412bc464
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-07-31 11:56:26 -0300

    Add Pages SignIn and SignOut

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,15 @@ import GlobaStyles from './styles/global';
 import theme from './styles/theme';
 
 import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobaStyles />      
-      <Home />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,41 @@
+import { Container, Form, BackgroundImg } from './styles';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+export function SignUp(){
+    return (
+        <Container>
+            <BackgroundImg />
+
+            <Form >
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar seus links úteis.</p>
+
+                <h2>Crie sua conta</h2>
+
+                <Input 
+                   placeholder='Nome' 
+                   type='text'
+                   icon={FiUser}
+                />
+                <Input 
+                   placeholder='E-mail' 
+                   type='text'
+                   icon={FiMail}
+                />
+                <Input 
+                   placeholder='Senha' 
+                   type='password'
+                   icon={FiLock}
+                />
+
+                <Button title='Cadastrar' />
+
+                <a href='#'>Voltar para o login</a> 
+            </Form>
+            
+        </Container>
+    );
+}
